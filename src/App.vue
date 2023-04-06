fc94ac597069eb2163a0e677b67befe4fbd54999
jyoung7988 <jyoung7988@naver.com>
2023-04-06 22:30:27 +0900

    이벤트 버스로 스피너 사용 - 이벤트 객체 off 로 제거해줘야함

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,43 @@
     <transition name="page">
       <router-view></router-view>
     </transition>  
+    <spinner-load :loading='loadingStatus'></spinner-load>
   </div>
 </template>
 
 <script>
 import ToolBar from './components/ToolBar.vue';
+import SpinnerLoad from './components/SpinnerLoad.vue';
+import bus from './utils/bus.js';
+
 export default {
   
   components: {
     ToolBar,
+    SpinnerLoad
   },
+  data(){
+    return{
+      loadingStatus: false,
+    };
+  },
+  methods:{
+    startSpinner(){
+      this.loadingStatus = true;
+    },
+    endSpinner(){
+      this.loadingStatus = false;
+    }
+  },
+  created(){
+    bus.$on('start:spinner', this.startSpinner);
+    bus.$on('end:spinner', this.endSpinner);
+
+  },
+  beforeDestroy(){
+    bus.$off('start:spinner', this.startSpinner);
+    bus.$off('end:spinner', this.endSpinner);
+  }
 }
 </script>
 
